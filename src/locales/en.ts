f6b74d1bebdcf805880508c3f286feadfa2a34e2
Magnetkopf <82427906+Magnetkopf@users.noreply.github.com>
2026-03-14 19:21:39 +0800

    i18n: init

---
+++ src/locales/en.ts
@@ -0,0 +1,11 @@
+export default {
+  home: {
+    error: "Error",
+    waitingData: "Waiting for server data...",
+    cpu: "CPU",
+    ram: "RAM",
+    load: "Load",
+    network: "Network",
+    disk: "Disk",
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createPersistedState } from "pinia-plugin-persistedstate";
+import { createI18n } from "vue-i18n";
+import en from "./locales/en";
 import App from "./App.vue";
 import router from "./router";
 import "./style/app.css";
 
 const app = createApp(App);
 const pinia = createPinia();
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: "en",
+  fallbackLocale: "en",
+  messages: {
+    en,
+  },
+});
 pinia.use(createPersistedState());
 
 app.use(pinia);
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
