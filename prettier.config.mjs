dae045b24cae22078608491fc42ddec5c67869fb
landmineHQ <z1216198007@gmail.com>
2026-05-23 16:12:38 +0800

    chrome: add format config for format

--- prettier.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindStylesheet: "./src/style/app.css",
-  tailwindFunctions: ["cn", "clsx", "cva"],
-};
